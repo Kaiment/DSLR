a2d9f2b565b65b59630bc07d6ccab95ad4220b86
Antoine Gaubert <angauber@z4r3p3.le-101.fr>
2020-02-14 16:33:43 +0100

    update scatter plot

--- srcs/scatter_plot.py
+++ srcs/scatter_plot.py
@@ -3,10 +3,25 @@ from helpers import *
 from matplotlib import pyplot
 
 if __name__ == "__main__":
-    data = dataToDict("../datasets/dataset_test.csv", 6)
-    colors = {"Arithmancy": "#eb4034", "Astronomy": "b","Herbology": "#eba834","Defense Against the Dark Arts": "#b1eb34","Divination": "#34eb8f","Muggle Studies": "#34d8eb","Ancient Runes": "#345eeb","History of Magic": "#345eeb","Transfiguration": "#eb34d3","Potions": "m","Care of Magical Creatures": "c","Charms": "g","Flying": "r"}
+    data = dataToDict("../datasets/dataset_train.csv", 6)
 
-    for key, val in data.items():
-        pyplot.scatter(range(0, len(val)), val, marker='x', color=colors[key])
-    pyplot.legend(colors.keys())
+    # pyplot.scatter(data["Arithmancy"], data["Astronomy"], marker='x', color="blue")
+    
+    i = 1
+    visited = []
+
+    for keyx, valx in data.items():
+        for keyy, valy in data.items():
+            if keyx != keyy and [keyx, keyy] not in visited and [keyy, keyx] not in visited:
+                visited.append([keyx, keyy])
+                pyplot.scatter(valx, valy, marker='x', color="purple")
+                pyplot.xlabel(keyx)
+                pyplot.ylabel(keyy)
+                pyplot.subplot(6, 13, i)
+                pyplot.tight_layout()
+                i += 1
+
+    print(len(visited))
+
+    # pyplot.legend(colors.keys())
     pyplot.show()
