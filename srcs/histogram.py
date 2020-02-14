8775feae64d8eee429b7cd696f5c9f35434f8524
Antoine Gaubert <angauber@z4r3p3.le-101.fr>
2020-02-14 15:08:59 +0100

    update histogram

--- srcs/histogram.py
+++ srcs/histogram.py
@@ -4,14 +4,14 @@ from matplotlib import pyplot
 
 if __name__ == "__main__":
     data = dataToDict("../datasets/dataset_test.csv", 6)
-
-    courseStds = {}
-
     metaDataDict = getMetadata(data, data.keys())
-    for key, values in metaDataDict.items():
-        courseStds[key] = 1 / values["std"]
-    print(courseStds)
-    pyplot.bar(courseStds.keys(), courseStds.values(), 0.3, color='g', align="edge")
-    pyplot.subplots_adjust(bottom=0.5)
-    pyplot.xticks(rotation=-90);
+    
+    colors = {"Arithmancy": "#eb4034", "Astronomy": "b","Herbology": "#eba834","Defense Against the Dark Arts": "#b1eb34","Divination": "#34eb8f","Muggle Studies": "#34d8eb","Ancient Runes": "#345eeb","History of Magic": "#345eeb","Transfiguration": "#eb34d3","Potions": "m","Care of Magical Creatures": "c","Charms": "g","Flying": "r"}
+
+    i = 1
+    for key, values in data.items():
+        pyplot.subplot(4, len(data.keys()) / 4 + 1, i)
+        i += 1
+        pyplot.hist(values, 25, color=colors[key], density=True, alpha=0.4)
+        pyplot.title(key+" => mean: "+str(round(metaDataDict[key]["mean"], 2))+", std: "+str(round(metaDataDict[key]["std"], 3)))
     pyplot.show()
