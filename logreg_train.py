f17b13f478a634195fe90011aa047a0d9d4469e3
Antoine Gaubert <angauber@z4r4p5.le-101.fr>
2020-02-21 16:46:24 +0100

    one vs all

--- logreg_train.py
+++ logreg_train.py
@@ -4,16 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class logisticRegression:
-    def __init__(self, dataset):
+    def __init__(self, dataset): # should take two courses
         self.dataset = dataset
-   
+        self.houses = ["Slytherin", "Ravenclaw", "Gryffindor", "Hufflepuff"]
+        self.results = np.array([])
+
     def read(self):
         data = helpers.dataToList(self.dataset)
         self.headers = data.pop(0)
 
         self.X_0 = self.headers.index("Herbology");
         self.X_1 = self.headers.index("Defense Against the Dark Arts")
-      
+
         rows = []
 
         for key, row in enumerate(data):
@@ -27,10 +29,6 @@ class logisticRegression:
         data = rows;
         self.data = data;
 
-        self.Y = np.array([[1] if row[self.headers.index("Hogwarts House")] == "Slytherin" else [0] for row in data])
-        self.X = np.array([[float(row[self.X_0]), float(row[self.X_1]), 1 if row[self.headers.index("Hogwarts House")] == "Slytherin"  else 0] for row in data])
-        self.theta = np.zeros((self.X.shape[1], 1))
-        
         return self
 
     def sigmoid(self, x):
@@ -51,15 +49,32 @@ class logisticRegression:
 
         return derivative
 
+    def learn(self):
+        # one vs all
+        for house in self.houses:
+            self.Y = np.array([[1] if row[self.headers.index("Hogwarts House")] == house else [0] for row in self.data])
+            self.X = np.array([[float(row[self.X_0]), float(row[self.X_1]), 1 if row[self.headers.index("Hogwarts House")] == house  else 0] for row in self.data])
+            self.theta = np.zeros((self.X.shape[1], 1))
+            self.train()
+            self.process_results()
+
     def train(self):
         learningRate = 0.0001
-        epoch = 40000
+        epoch = 50000
         for key, e in enumerate(range(epoch)):
             if (key % 100 == 0):
                 cost = self.cost(self.theta, self.X, self.Y)
                 print("cost: "+str(cost))
             self.theta -= learningRate * self.compute_theta(self.theta, self.X, self.Y)
         return self
+
+    def process_results(self):
+        predicted = self.predict(self.theta, self.X)
+
+        if len(self.results) > 0:
+            self.results = np.append(self.results, predicted, axis = 1)
+        else:
+            self.results = predicted
 
     def print_predictions(self):
         predicted = self.predict(self.theta, self.X)
@@ -82,14 +97,14 @@ class logisticRegression:
 
         plt.scatter(trueSet[:,0], trueSet[:,1], color="green")
         plt.scatter(falseSet[:,0], falseSet[:,1], color="red")
-        
+
         plt.title("scatter plot Houses")
         plt.xlabel("Herbology")
         plt.ylabel("Defense Against The Dark arts")
         plt.show()
         return self
 
-    def compute_precision(self):
+    def compute_accuracy(self):
         predicted = self.predict(self.theta, self.X)
         correct = 0
         total = self.X.shape[0]
@@ -108,4 +123,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     logisticObj = logisticRegression(args.dataset)
-    logisticObj.read().train().compute_precision()
+    logisticObj.read().learn()
+
+    print(logisticObj.results[0]) # probability for first student to be in each school (pick the highest) WARNING first student here might not be first one in the dataset
